c16d69fbcf985dc66dc631b4c9ba747ec459935c
Osvaldas <osvaldas.gapsys@codeacademylt.onmicrosoft.com>
2024-01-25 19:19:17 +0100

    UPDATED

--- routers/store.py
+++ routers/store.py
@@ -16,7 +16,17 @@ router = APIRouter(
     tags=["Store"],
 )
 
-@router.post('/{advert_package_id}', response_model=schemas.UserOut)
+@router.get('/', response_model=List[schemas.AdvertPackage])
+async def get_packages(request: Request,current_user: int = Depends(oauth2.get_current_user) , db: Session = Depends(get_db)):
+    packages = db.query(models.AdvertPackages).all()
+    user_packages = []
+    for advert_package in packages:
+        if advert_package in current_user.advert_packages:
+            user_packages.append(advert_package)
+
+    return templates.TemplateResponse(name="store.html", context={"request": request, "packages": packages, "user_packages": user_packages, "current_user": current_user})
+
+@router.post('/{advert_package_id}')
 async def buy_advert_package(advert_package_id: int, current_user: int = Depends(oauth2.get_current_user), db: Session=Depends(get_db)):
     
     advert_package = db.query(models.AdvertPackages).filter(models.AdvertPackages.id == advert_package_id).first()
@@ -40,9 +50,4 @@ async def buy_advert_package(advert_package_id: int, current_user: int = Depends
     db.commit()
     db.refresh(user)
 
-    return schemas.UserOut(email=user.email, credits=user.credits, packages=user.advert_packages)
-
-@router.get('/packages', response_model=List[schemas.AdvertPackage])
-async def get_packages(request: Request, db: Session = Depends(get_db)):
-    packages = db.query(models.AdvertPackages).all()
-    return templates.TemplateResponse(name="store.html", context={"request": request, "packages": packages })
+    return {"message": "Package bought successfully"}
