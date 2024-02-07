740fcbff11be8326bf17b2ad850dd7072c4dff8a
Matas Vitkauskas <matasr1337@gmail.com>
2024-02-07 19:55:59 +0200

    Small changes to code.

--- scraper.py
+++ scraper.py
@@ -13,10 +13,17 @@ from urllib.parse import urlparse
 class SCRAPER:
     def __init__(self, url):
         self.url = url
-        self.manager = ChromeService(ChromeDriverManager().install()) #Chrome manageris
-        self.driver = webdriver.Chrome(service=self.manager) #chrome driveris
+        self.manager = ChromeService(ChromeDriverManager().install())
+        self.options = webdriver.ChromeOptions() 
+        self.options.add_argument(r"load-extension=C:\Users\matas\AppData\Local\Google\Chrome\User Data\Default\Extensions\cjpalhdlnbpafiamejdnhcphjbkeiagm\1.55.0_0")
+        self.driver = webdriver.Chrome(service=self.manager, options=self.options)
         self.driver.get(self.url)
         self.db = SessionLocal()
+        self.a = 0
+        self.max_iterations = 200
+        self.iteration = 0
+        self.photo_counter = 1
+
 
     def measure_time(func):
         def wrapper(*args, **kwargs):
@@ -35,14 +42,14 @@ class SCRAPER:
         except:
             pass
 
-    def list_of_urls(self):
+    def compare_and_retrieve_urls(self):
         self.list_urls = []
-        a = 0
-        if a == 0:
+        if self.a == 0:
             self.click_cookie()
-            a+=1
+            self.a+=1
         else:
             pass
+        
         advert_ids_in_database = {str(advert_id[0]) for advert_id in self.db.query(CarAdverts.advert_id).all()}
         pages = self.driver.find_elements('xpath', xPath.LIST_ITEMS)
         for page in pages:
@@ -51,46 +58,45 @@ class SCRAPER:
                 self.list_urls.append(page.get_attribute('href') + '#gallery')
         return self.list_urls
 
-    def pull_car_info_mobile(self):
+    def retrieve_car_info(self):
         marke, modelis = self.driver.find_element('xpath', "//h1").text.split(' ', 1)
-        list_of_set = {
+        set_of_car_info = {
             'make': marke,
             'model': modelis,
-            'phone_number': self.try_finding_element_withoutsplitting(xPath.CONTACT_PHONE),
-            'contact_name': self.try_finding_element_withoutsplitting(xPath.CONTACT_NAME),
-            'city': self.try_finding_element_withoutsplitting(xPath.CONTACT_LOCATION),
-            'price': ''.join(filter(str.isdigit, self.try_finding_element_withoutsplitting(xPath.MAIN_PRICE))),
-            'year': self.try_finding_element(xPath.MAKE_DATE)[:4] if len(self.try_finding_element(xPath.MAKE_DATE)) > 4 else self.try_finding_element(xPath.MAKE_DATE),
-            'first_registration': self.try_finding_element(xPath.ORIGIN_COUNTRY_ID),
-            'engine_type': self.try_finding_element(xPath.ENGINE),
-            'body_type': self.try_finding_element(xPath.BODY_TYPE_ID),
-            'wheels_size': self.try_finding_element(xPath.WHEEL_DRIVE_ID),
-            'climate_control': self.try_finding_element(xPath.CONDITION_TYPE_ID),
-            'defects': self.try_finding_element(xPath.HAS_DAMAGED_ID),
-            'mileage': ''.join(filter(str.isdigit, self.try_finding_element(xPath.KILOMETRAGE))) if self.try_finding_element(xPath.KILOMETRAGE) != "none" else 0,
-            'fuel_type': self.try_finding_element(xPath.FUEL_ID),
-            'gearbox': self.try_finding_element(xPath.GEARBOX_ID),
-            'color': self.try_finding_element(xPath.COLOR_ID),
-            'advert_id': self.try_finding_element(xPath.ID)[1:],
+            'phone_number': self.find_element_wo_split(xPath.CONTACT_PHONE),
+            'contact_name': self.find_element_wo_split(xPath.CONTACT_NAME),
+            'city': self.find_element_wo_split(xPath.CONTACT_LOCATION),
+            'price': ''.join(filter(str.isdigit, self.find_element_wo_split(xPath.MAIN_PRICE))),
+            'year': self.find_element_w_split(xPath.MAKE_DATE)[:4] if len(self.find_element_w_split(xPath.MAKE_DATE)) > 4 else self.find_element_w_split(xPath.MAKE_DATE),
+            'first_registration': self.find_element_w_split(xPath.ORIGIN_COUNTRY_ID),
+            'engine_type': self.find_element_w_split(xPath.ENGINE),
+            'body_type': self.find_element_w_split(xPath.BODY_TYPE_ID),
+            'wheels_size': self.find_element_w_split(xPath.WHEEL_DRIVE_ID),
+            'climate_control': self.find_element_w_split(xPath.CONDITION_TYPE_ID),
+            'defects': self.find_element_w_split(xPath.HAS_DAMAGED_ID),
+            'mileage': ''.join(filter(str.isdigit, self.find_element_w_split(xPath.KILOMETRAGE))) if self.find_element_w_split(xPath.KILOMETRAGE) != "none" else 0,
+            'fuel_type': self.find_element_w_split(xPath.FUEL_ID),
+            'gearbox': self.find_element_w_split(xPath.GEARBOX_ID),
+            'color': self.find_element_w_split(xPath.COLOR_ID),
+            'advert_id': self.find_element_w_split(xPath.ID)[1:],
             'url': self.driver.current_url
         }
 
-                 
         try:
-            self.db.add(CarAdverts(**list_of_set))
+            self.db.add(CarAdverts(**set_of_car_info))
             self.db.commit()
             self.download_photos()
         finally:
             self.db.close()
-        return list_of_set
+        return set_of_car_info
 
-    def try_finding_element_withoutsplitting(self, xpathas, default_value = "none"):
+    def find_element_wo_split(self, xpathas, default_value = "none"):
         try:
             return self.driver.find_element('xpath', xpathas).text
         except:
             return default_value
         
-    def try_finding_element(self, xpathas, default_value = "none"):
+    def find_element_w_split(self, xpathas, default_value = "none"):
         try:
             return self.driver.find_element('xpath', xpathas).text.split(': ', 1)[1]
         except:
@@ -106,7 +112,7 @@ class SCRAPER:
         return piclist
 
 
-    def next_page_mobile(self):
+    def next_page_button(self):
         try:
             url = self.driver.find_element('xpath', xPath.NEXT_PAGE_BUTTON).get_attribute('href')
         except NoSuchElementException:
@@ -114,23 +120,21 @@ class SCRAPER:
         return url
 
     @measure_time
-    def iterating_pages(self):
-        max_iterations = 200
-        iteration = 0
+    def run(self):
         while True:
-            self.list_of_urls()
+            self.compare_and_retrieve_urls()
             for link in self.list_urls:
                 self.driver.execute_script("window.open('about:blank', 'newtab')")
                 self.driver.switch_to.window(self.driver.window_handles[-1])
                 self.driver.get(link)
-                self.pull_car_info_mobile()
+                self.retrieve_car_info()
                 self.driver.switch_to.window(self.driver.window_handles[0])
-            next_page_url = self.next_page_mobile()
+            next_page_url = self.next_page_button()
             if not next_page_url:
                 break
             self.driver.get(next_page_url)
-            iteration += 1
-            if iteration >= max_iterations:
+            self.iteration += 1
+            if self.iteration >= self.max_iterations:
                 break
         print('Done!')
 
@@ -139,18 +143,17 @@ class SCRAPER:
         advert_id = os.path.basename(url_parts.path).split('-')[-1].split('.')[0]
         folder_name = 'static/Skelbimu_Images/' +  advert_id
         os.makedirs(folder_name, exist_ok=True)
-        photo_counter = 1
         for link in self.find_photos():
             try:
                 r = requests.get(link)
-                filename = os.path.join(folder_name, f"{photo_counter}.jpg")  # Use the counter in the filename
+                filename = os.path.join(folder_name, f"{self.photo_counter}.jpg")
                 with open(filename, 'wb') as f:
                     f.write(r.content)
-                photo_counter += 1  # Increment the counter
+                self.photo_counter += 1  
             except requests.exceptions.RequestException as e:
                 continue
 
 
 url = input('Iveskite URL: ')
 scraper = SCRAPER(url)
-scraper.iterating_pages()
+scraper.run()
